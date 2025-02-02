944a299f85e518419501ad3f947910d18b55ca7b
FEDERICO <federico.saggiro@icloud.com>
2025-02-02 20:57:55 +0100

    si

--- public/index.js
+++ public/index.js
@@ -1,108 +1,28 @@
-const template = `<li class="list-group-item %BG">
-   <span>%TODO</span>
-   <button id="delete_ID" type="button" class="todo btn btn-danger float-end">X</button>
-   <button id="success_ID" type="button" class="todo btn btn-success float-end">V</button>
-   </li>`;
-
 let todos = [];
-const todoInput = document.getElementById("input");
-const insertButton = document.getElementById("insertButton");
-const list = document.getElementById("list");
-
-const send = (todo) => {
-    return new Promise((resolve, reject) => {
-        fetch("/todo/add", {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(todo)
-        })
-        .then((response) => response.json())
-        .then((json) => {
-            resolve(json); // risposta del server all'aggiunta
-        })
-    })
-}
-
-const completeTodo = (todo) => {
-    return new Promise((resolve, reject) => {
-        fetch("/todo/complete", {
-            method: 'PUT',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(todo)
-        })
-        .then((response) => response.json())
-        .then((json) => {
-            resolve(json);
-        })
-    })
-}
- 
-const load = () => {
-    return new Promise((resolve, reject) => {
-        fetch("/todo")
-        .then((response) => response.json())
-        .then((json) => {
-            resolve(json); // risposta del server con la lista
-        })
-    })
-}
-
-const deleteTodo = (id) => {
-    return new Promise((resolve, reject) => {
-        fetch("/todo/"+id, {
-            method: 'DELETE',
-            headers: {
-                "Content-Type": "application/json"
-            },
-        })
-        .then((response) => response.json())
-        .then((json) => {
-            resolve(json);
-        })
-    })
-}
-
-insertButton.onclick = () => {
-    const todo = {
-        name: todoInput.value,
-        completed: false
-    }
-    send({todo: todo}) // 1. invia la nuova Todo
-    .then(() => load()) // 2. caricala nuova lista
-    .then((json) => {
-        todos = json.todos;
-        todoInput.value = "";
-        render();  // 3. render della nuova lista
-    });
-}
 
 const render = () => {
-    const todoList = document.getElementById("list");
-    todoList.innerHTML = "";
-    todos.forEach(todo => {
-        const li = `
-            <li class="${todo.completed ? 'completed' : ''}">
-                <span>${todo.name}</span>
-                <div>
-                    <button class="complete-button" id="${todo.id}">Completa</button>
-                    <button class="delete-button" id="${todo.id}">Elimina</button>
-                </div>
-            </li>
-        `;
-        todoList.innerHTML += li;
-    });
-    const completeButtons = document.querySelectorAll('.complete-button');
+   const todoList = document.getElementById("todoList");
+   todoList.innerHTML = "";
+   todos.forEach(todo => {
+       const li = `
+           <li class="${todo.completed ? 'completed' : ''}">
+               <span>${todo.name}</span>
+               <div>
+                   <button class="complete-button" id="${todo.id}">Completa</button>
+                   <button class="delete-button" id="${todo.id}">Elimina</button>
+               </div>
+           </li>
+       `;
+       todoList.innerHTML += li;
+   });
+   const completeButtons = document.querySelectorAll('.complete-button');
     completeButtons.forEach(button => {
         button.onclick = () => {
             const id = button.getAttribute('id');
             completeTodo(id);
         };
     });
-    
+
     const deleteButtons = document.querySelectorAll('.delete-button');
     deleteButtons.forEach(button => {
         button.onclick = () => {
@@ -112,12 +32,56 @@ const render = () => {
     });
 };
 
-const refresh = () => {
-    load().then((json) => {
-        todos = json.todos;
-        render();
+const send = (todo) => {
+    return fetch("/todo/add", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ todo })
+    }).then(response => response.json());
+};
+
+const load = () => {
+    return fetch("/todo")
+        .then(response => response.json())
+        .then(json => {
+            todos = json.todos;
+            render();
+        });
+};
+
+const completeTodo = (id) => {
+    const todo = { id };
+    return fetch("/todo/complete", {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(todo)
+    }).then(() => load());
+};
+
+const deleteTodo = (id) => {
+    return fetch("/todo/"+id, {
+        method: 'DELETE'
+    }).then(() => load());
+};
+
+document.getElementById("insertButton").onclick = () => {
+    const todoInput = document.getElementById("todoInput");
+    const todo = {
+        name: todoInput.value,
+        completed: false
+    };
+    send(todo).then(() => {
+        todoInput.value = "";
+        load();
     });
-}
- 
-refresh();
-setInterval(refresh, 30000);
+};
+
+load();
+
+setInterval(() => {
+   load();
+}, 30000);
--- server.js
+++ server.js
@@ -1,50 +1,48 @@
+let http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
-const http = require("http");
+const path = require("path");
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+   extended: true
 }));
 
-const path = require('path');
 app.use("/", express.static(path.join(__dirname, "public")));
 
-const todos = [];
+let todos = [];
 
 app.post("/todo/add", (req, res) => {
-   const todo = req.body.todo;
-   todo.id = "" + new Date().getTime();
-   todos.push(todo);
-   res.json({result: "Ok"});
+    const todo = req.body.todo;
+    todo.id = Date.now().toString();
+    todos.push(todo);
+    res.json({ result: "Ok" });
 });
 
 app.get("/todo", (req, res) => {
-   res.json({todos: todos});
+    res.json({ todos: todos });
 });
 
 app.put("/todo/complete", (req, res) => {
     const todo = req.body;
-    try {
-        todos = todos.map((element) => {
-            if (element.id === todo.id) {
-                element.completed = true;
-            }
-            return element;
-        })
-    } catch (e) {
-        console.log(e);
-    }
-    res.json({result: "Ok"});
+    todos = todos.map((element) => {
+        if (element.id === todo.id) {
+            element.completed = true;
+        }
+        return element;
+    });
+    res.json({ result: "Ok" });
 });
 
 app.delete("/todo/:id", (req, res) => {
     todos = todos.filter((element) => element.id !== req.params.id);
-    res.json({result: "Ok"});  
-})
+    res.json({ result: "Ok" });
+});
 
 const server = http.createServer(app);
-server.listen(80, () => {
-    console.log("- server running");
+
+server.listen(100, () => {
+    console.log(`- server running`);
 });
